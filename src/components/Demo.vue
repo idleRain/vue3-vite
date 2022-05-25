856921f99ecda3db10bb189b332be510540d0613
idleRain <gold.experience@foxmail.com>
2022-05-25 21:53:10 +0800

    day4

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,67 +1,24 @@
 <template>
   <h2>当前的数字： {{ num }}</h2>
   <button @click="num++">点我加一</button>
+  <h2>当前点击的鼠标坐标是—— x：{{point.x}}-y:{{point.y}}</h2>
 </template>
 
 <script>
-import {ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted} from "vue";
+import {ref} from "vue";
+// 引入自定义 hook
+import usePoint from '../hooks/usePoint.js'
 
 export default {
   name: "Demo",
   setup() {
     let num = ref(0)
-    // 组合式 API 使用生命周期钩子， 并且执行顺序永远在配置项式生命周期钩子前面
-    // 原 beforeCreate 与 created 在组合式 API 中已被 setup 取代
-    console.log('--- setup ---')
-    onBeforeMount(() => {
-      console.log('--- onBeforeMount ---')
-    })
-    onMounted(() => {
-      console.log('--- onMounted ---')
-    })
-    onBeforeUpdate(() => {
-      console.log('--- onBeforeUpdate ---')
-    })
-    onUpdated(() => {
-      console.log('--- onUpdated ---')
-    })
-    onBeforeUnmount(() => {
-      console.log('--- onBeforeUnmount ---')
-    })
-    onUnmounted(() => {
-      console.log('--- onUnmounted ---')
-    })
-
+    const point = usePoint()
+    debugger
     return {
-      num
+      num, point
     }
-  },
-  // 通过配置项的形式使用生命周期钩子
-  /*beforeCreate() {
-    console.log('--- beforeCreate ---')
-  },
-  created() {
-    console.log('--- created ---')
-  },
-  beforeMount() {
-    console.log('--- beforeMount ---')
-  },
-  mounted() {
-    console.log('--- mounted ---')
-  },
-  beforeUpdate() {
-    console.log('--- beforeUpdate ---')
-  },
-  updated() {
-    console.log('--- updated ---')
-  },
-  // 注意 ：原 beforeDestroy 、 destroyed 已更名 beforeUnmount 、 unmounted
-  beforeUnmount() {
-    console.log('--- beforeUnmount ---')
-  },
-  unmounted() {
-    console.log('--- unmounted ---')
-  }*/
+  }
 }
 </script>
 
---
+++ src/hooks/usePoint.js
@@ -0,0 +1,26 @@
+import {reactive, onMounted, onBeforeUnmount} from "vue";
+// 自定义 hook ，类似于 mixin混入
+export default () => {
+    // 页面打点相关的数据
+    const point = reactive({
+        x: 0,
+        y: 0
+    })
+    // 页面打点相关的函数
+    const savePoint = e => {
+        point.x = e.pageX
+        point.y = e.pageY
+        console.log(point.x, point.y)
+    }
+    // 组件挂载后给 window 绑定点击事件
+    onMounted(() => {
+        window.addEventListener('click', savePoint)
+    })
+    // 组件销毁前取消绑定事件
+    onBeforeUnmount(() => {
+        window.removeEventListener('click', savePoint)
+    })
+
+    // 将数据返回出去
+    return point
+}
