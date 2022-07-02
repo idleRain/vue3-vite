c969043f9682163fda2721a9a38c6ae51b7d2da1
idleRain <gold.experience@foxmail.com>
2022-07-02 21:39:01 +0800

    update

--- 01_src_setup/App.vue
+++ 01_src_setup/App.vue
@@ -1,38 +1,28 @@
+<template>
+  <Demo message="你好啊" school="麻省理工大学" @hello="showMsg">
+    <template v-slot:haha>
+      <p>确实帅</p>
+    </template>
+  </Demo>
+</template>
+
 <script>
-import {ref} from "vue";
+import Demo from "./components/Demo.vue"
 
 export default {
   name: 'App',
-  // 数据和函数包裹在 setup() 中
-  setup(){
-    // 基本数据类型用 ref() 定义 , 返回一个 ref 类型的对象
-    const name = ref('小丁')
-    const age = ref(18)
-    const job = ref({
-      type: '前端工程师',
-      salary: '20k'
-    })
-    function setInfo() {
-      // 修改数据不用 this , 要用 xxx.value
-      name.value = '小锋'
-      age.value = 19
-      job.value.salary = '30k'
+  components: {
+    Demo
+  },
+  setup() {
+    const showMsg = msg => {
+      alert(msg)
     }
-    // 定义的变量和函数需要返回
-    return {
-      name, age, setInfo, job
-    }
+    return {showMsg}
   }
 }
 </script>
 
-<template>
-  <h1>你好</h1>
-  <h2>name: {{name}}</h2>
-  <h2>age: {{age}}</h2>
-  <h3 v-for="(j, index) in job" :key="index">{{j}}</h3>
-  <button @click="setInfo">点我点我</button>
-</template>
+<style scoped>
 
-<style>
 </style>
---
+++ 04_src_watch/App.vue
@@ -0,0 +1,51 @@
+<template>
+</template>
+
+<script setup>
+import {watch, ref} from "vue";
+// 监听单个ref
+const money1 = ref(100)
+watch(money1, (value, oldValue) => {
+  console.log(value)
+})
+
+// 监听多个ref
+const money2 = ref(100)
+const count = ref(0)
+watch([money2, count], (value) => {
+  console.log(value)
+})
+
+// 监听ref复杂数据
+const user1 = ref({
+  name: 'zs',
+  age: 18,
+})
+watch(
+    user1,
+    (value) => {
+      console.log('user变化了', value)
+    },
+    {
+      // 深度监听，，，当ref的值是一个复杂数据类型，需要深度监听
+      deep: true,
+      immediate: true
+    }
+)
+
+// 监听对象的某个属性的变化
+const user2 = ref({
+  name: 'zs',
+  age: 18,
+})
+watch(
+    () => user2.value.name,
+    (value) => {
+      console.log(value)
+    }
+)
+</script>
+
+<style scoped>
+
+</style>
